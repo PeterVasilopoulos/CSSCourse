cb601403f9d515f9ca083a3c9c5135244862a1d2
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-12-09 11:49:23 -0500

    finished section 9

--- 9-JavaScript and CSS/shared.js
+++ 9-JavaScript and CSS/shared.js
@@ -6,24 +6,44 @@ let modal = document.querySelector('.modal');
 let selectPlanButton = document.querySelectorAll('.plan button');
 // no button element
 let noButton = document.querySelector('.modal__actions button');
+// toggle button
+let toggleButton = document.querySelector('.toggle-button');
+// mobile nav
+let mobileNav = document.querySelector('.mobile-nav');
 
 // show backdrop and modal function
 const showOverlay = () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
 }
 
-// hide backdrop and modal function
+// hide backdrop and modal function and side nav
 const hideOverlay = () => {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    if(modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+    mobileNav.classList.remove('open');
 }
 
-// add event listeners to 'choose plan' buttons to show overlay
+// event listeners to 'choose plan' buttons to show overlay
 for(let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', showOverlay);
 }
 
-// add event listener to no button and backdrop to remove overlay
+// event listener to no button and backdrop to remove overlay
 backdrop.addEventListener('click', hideOverlay);
-noButton.addEventListener('click', hideOverlay);
+if(noButton) {
+    noButton.addEventListener('click', hideOverlay);
+}
+
+// event listener to open side nav
+toggleButton.addEventListener('click', function() {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
